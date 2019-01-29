f999d193ac31cfd9ffcb26e043bc10eaf48e93fc
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:26 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostSortConan(base.BoostBaseConan):
     name = "boost_sort"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_sort"
     lib_short_names = ["sort"]
     header_only_libs = ["sort"]
