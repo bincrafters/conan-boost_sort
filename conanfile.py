b27ae9e520ba4b55aee1459fba57b24efb6dfc6f
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,19 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostSortConan(base.BoostBaseConan):
     name = "boost_sort"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_sort"
-    lib_short_names = ["sort"]
-    header_only_libs = ["sort"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_range",
-        "boost_serialization",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
